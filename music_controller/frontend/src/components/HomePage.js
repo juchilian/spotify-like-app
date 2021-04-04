11d9a9e55d4fac5cbb62202dff21cdeee503dacd
juchilian <juchilian1227@gmail.com>
2021-04-04 19:26:45 +0900

    Tutorial#10

--- music_controller/frontend/src/components/HomePage.js
+++ music_controller/frontend/src/components/HomePage.js
@@ -17,6 +17,7 @@ export default class HomePage extends Component {
     this.state = {
       roomCode: null,
     }
+    this.clearRoomCode = this.clearRoomCode.bind(this)
   }
     
   async componentDidMount() {
@@ -49,6 +50,12 @@ export default class HomePage extends Component {
     );
   }
 
+  clearRoomCode() {
+    this.setState({
+      roomCode: null,
+    })
+  }
+
   render() {
     return (
       <Router>
@@ -56,11 +63,14 @@ export default class HomePage extends Component {
           <Route exact path="/" render={() => {
             return this.state.roomCode ? (<Redirect to={`/room/${this.state.roomCode}`}/>) : this.renderHomePage()
           }}>
-            {this.renderHomePage()}
           </Route>
           <Route path="/join" component={RoomJoinPage} />
           <Route path="/create" component={CreateRoomPage} />
-          <Route path="/room/:roomCode" component={Room} />
+          <Route path="/room/:roomCode"
+            render={(props) => {
+              return <Room {...props} leaveRoomCallback={this.clearRoomCode} />
+            }}
+          />
         </Switch>
       </Router>
     );
