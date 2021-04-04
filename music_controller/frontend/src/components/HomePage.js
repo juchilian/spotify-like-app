774d9701b335484c972fff45694cb3e2d4676a5a
juchilian <juchilian1227@gmail.com>
2021-04-04 18:26:45 +0900

    Tutorial#10

--- music_controller/frontend/src/components/HomePage.js
+++ music_controller/frontend/src/components/HomePage.js
@@ -1,26 +1,68 @@
-import React, { Component } from 'react'
-import RoomJoinPage from "./RoomJoinPage"
-import CreateRoomPage from "./CreateRoomPage"
-import { BrowserRouter as Router, Route, Link, Redirect, Switch } from "react-router-dom"
-import Room from './Room';
+import React, { Component } from "react";
+import RoomJoinPage from "./RoomJoinPage";
+import CreateRoomPage from "./CreateRoomPage";
+import Room from "./Room";
+import { Button, ButtonGroup, Grid, Typography } from "@material-ui/core";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect,
+  Switch,
+} from "react-router-dom";
 
 export default class HomePage extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
+    this.state = {
+      roomCode: null,
     }
+  }
+    
+  async componentDidMount() {
+    fetch('/api/user-in-room').then((response) => response.json()).then((data) => {
+      this.setState({
+        roomCode: data.code,
+      });
+    })
+  }
 
-    render() {
-        return (
-            <Router>
-                <Switch>
-                    <Route exact path='/'>
-                        <p>This is the home page</p>
-                    </Route>
-                    <Route path='/join' component={RoomJoinPage} />
-                    <Route path='/create' component={CreateRoomPage} />
-                    <Route path='/room/:roomCode' component={Room} />
-                </Switch>
-            </Router>
-        )
-    }
+  renderHomePage() {
+    return (
+      <Grid container spacing={3}>
+          <Grid item xs={12} align="center">
+            <Typography variant="h3" component="h3">
+                House Party
+            </Typography>
+          </Grid>
+          <Grid item xs={12} align="center">
+            <ButtonGroup disableElevation variant="contained" color="primary">
+              <Button color="primary" to="/join" component={Link}>
+                Join a Room
+              </Button>
+              <Button color="secondary" to="/create" component={Link}>
+                Create a Room
+              </Button>
+            </ButtonGroup>
+          </Grid>
+      </Grid>
+    );
+  }
+
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" render={() => {
+            return this.state.roomCode ? (<Redirect to={`/room/${this.state.roomCode}`}/>) : this.renderHomePage()
+          }}>
+            {this.renderHomePage()}
+          </Route>
+          <Route path="/join" component={RoomJoinPage} />
+          <Route path="/create" component={CreateRoomPage} />
+          <Route path="/room/:roomCode" component={Room} />
+        </Switch>
+      </Router>
+    );
+  }
 }
