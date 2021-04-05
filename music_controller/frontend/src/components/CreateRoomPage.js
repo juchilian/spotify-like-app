ad9a6453c95f561dc4cdc45e626aacd2530a64f7
juchilian <juchilian1227@gmail.com>
2021-04-05 10:40:21 +0900

    Tutorial#12

--- music_controller/frontend/src/components/CreateRoomPage.js
+++ music_controller/frontend/src/components/CreateRoomPage.js
@@ -1,26 +1,41 @@
 import React, { Component } from "react";
-import Button from "@material-ui/core/Button";
-import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
 import { Link } from "react-router-dom";
-import Radio from "@material-ui/core/Radio";
-import RadioGroup from "@material-ui/core/RadioGroup";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import { FormControl, FormHelperText, TextField } from "@material-ui/core";
+import {
+  Grid,
+  Collapse,
+  FormControl,
+  FormHelperText,
+  Radio,
+  RadioGroup,
+  FormControlLabel,
+  TextField,
+  Typography,
+  Button,
+} from "@material-ui/core";
+import { Alert } from "@material-ui/lab/Alert"
 
 export default class CreateRoomPage extends Component {
-  defaultVotes = 2;
+  static defaultProps = {
+    votesToSkip: 2,
+    guestCanPause: true,
+    update: false,
+    roomCode: null,
+    updateCallback: () => {},
+  };
 
   constructor(props) {
     super(props);
     this.state = {
-      guestCanPause: true,
-      votesToSkip: this.defaultVotes,
+      guestCanPause: this.props.guestCanPause,
+      votesToSkip: this.props.votesToSkip,
+      errorMsg: "",
+      successMsg: "",
     };
 
     this.handleRoomButtonPressed = this.handleRoomButtonPressed.bind(this);
     this.handleVotesChange = this.handleVotesChange.bind(this);
     this.handleGuestCanPauseChange = this.handleGuestCanPauseChange.bind(this);
+    this.handleUpdateButtonPressed = this.handleUpdateButtonPressed.bind(this);
   }
 
   handleVotesChange(e) {
@@ -49,12 +64,99 @@ export default class CreateRoomPage extends Component {
       .then((data) => this.props.history.push("/room/" + data.code)); // redirect to room page
   }
 
-  render() {
+  renderCreateButtons() {
     return (
       <Grid container spacing={1}>
         <Grid item xs={12} align="center">
-          <Typography component="h4" variant="h4">
+          <Button
+            color="primary"
+            variant="contained"
+            onClick={this.handleRoomButtonPressed}
+          >
             Create A Room
+          </Button>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Button color="secondary" variant="contained" to="/" component={Link}>
+            Back
+          </Button>
+        </Grid>
+      </Grid>
+    );
+  }
+
+  renderUpdateButtons() {
+    return (
+      <Grid container spacing={1}>
+        <Grid item xs={12} align="center">
+          <Button
+            color="primary"
+            variant="contained"
+            onClick={this.handleUpdateButtonPressed}
+          >
+            Update Room
+          </Button>
+        </Grid>
+      </Grid>
+    );
+  }
+
+  handleUpdateButtonPressed() {
+    const requestOptions = {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        votes_to_skip: this.state.votesToSkip,
+        guest_can_pause: this.state.guestCanPause,
+        code: this.props.roomCode,
+      }),
+    };
+    fetch("/api/update-room", requestOptions).then((response) => {
+      if (response.ok) {
+        this.setState({
+          successMsg: "Room updated successfully!",
+        });
+      } else {
+        this.setState({
+          errorMsg: "Error updating room...",
+        });
+      }
+      this.props.updateCallback()
+    });
+  }
+  render() {
+    const title = this.props.update ? "Update Room" : "Create a Room";
+
+    return (
+      <Grid container spacing={1}>
+        <Grid item xs={12} align="center">
+          <Collapse
+            in={this.state.errorMsg != "" || this.state.successMsg != ""}
+          >
+            {this.state.successMsg != "" ? (
+              <Alert
+                severity="success"
+                onClose={() => {
+                  this.setState({ successMsg: "" });
+                }}
+              >
+                {this.state.successMsg}
+              </Alert>
+            ) : (
+              <Alert
+                severity="error"
+                onClose={() => {
+                  this.setState({ errorMsg: "" });
+                }}
+              >
+                {this.state.errorMsg}
+              </Alert>
+            )}
+          </Collapse>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Typography component="h4" variant="h4">
+            {title}
           </Typography>
         </Grid>
         <Grid item xs={12} align="center">
@@ -64,7 +166,7 @@ export default class CreateRoomPage extends Component {
             </FormHelperText>
             <RadioGroup
               row
-              defaultValue="true"
+              defaultValue={this.props.guestCanPause.toString()}
               onChange={this.handleGuestCanPauseChange}
             >
               <FormControlLabel
@@ -87,7 +189,7 @@ export default class CreateRoomPage extends Component {
                 required={true}
                 type="number"
                 onChange={this.handleVotesChange}
-                defaultValue={this.defaultVotes}
+                defaultValue={this.state.votesToSkip}
                 inputProps={{
                   min: 1,
                   style: { textAlign: "center" },
@@ -99,20 +201,9 @@ export default class CreateRoomPage extends Component {
             </FormControl>
           </Grid>
         </Grid>
-        <Grid item xs={12} align="center">
-          <Button
-            color="primary"
-            variant="contained"
-            onClick={this.handleRoomButtonPressed}
-          >
-            Create A Room
-          </Button>
-        </Grid>
-        <Grid item xs={12} align="center">
-          <Button color="secondary" variant="contained" to="/" component={Link}>
-            Back
-          </Button>
-        </Grid>
+        {this.props.update
+          ? this.renderUpdateButtons()
+          : this.renderCreateButtons()}
       </Grid>
     );
   }
